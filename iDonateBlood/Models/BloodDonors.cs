f7b89719d11397910aab78b561cc66e3c79cb218
Ajay Jadhav <ajayjadhav@yahoo.com>
2012-09-21 19:48:40 +0530

    MobileServices table & code added

---
+++ iDonateBlood/Models/BloodDonors.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iDonateBlood.Models
+{
+    public class BloodDonors
+    {
+
+        public int Id { get; set; }
+
+        [DataMember(Name = "email", IsRequired=true)]
+        public string Email { get; set; }
+
+        [DataMember(Name = "password", IsRequired = true)]
+        public string Password { get; set; }
+
+        [DataMember(Name = "fullname", IsRequired = true)]
+        public string FullName { get; set; }
+
+        [DataMember(Name = "dob", IsRequired = true)]
+        public DateTime DateOfBirth { get; set; }
+
+        [DataMember(Name = "bloodgroup", IsRequired = true)]
+        public string BloodGroup { get; set; }
+
+        [DataMember(Name = "lastdonatedon")]
+        public DateTime LastDonatedOn { get; set; }
+
+        [DataMember(Name = "neverdonated")]
+        public bool NevenDonated { get; set; }
+
+        [DataMember(Name = "mobileno", IsRequired = true)]
+        public string MobileNumber { get; set; }
+
+        [DataMember(Name = "telephoneno")]
+        public string TelephoneNumber { get; set; }
+
+        [DataMember(Name = "address", IsRequired = true)]
+        public string Address { get; set; }
+
+        [DataMember(Name = "country", IsRequired = true)]
+        public string Country { get; set; }
+
+        [DataMember(Name = "state", IsRequired = true)]
+        public string State { get; set; }
+
+        [DataMember(Name = "city", IsRequired = true)]
+        public string City { get; set; }
+
+    }
+}
--- iDonateBlood/Register.xaml.cs
+++ iDonateBlood/Register.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using iDonateBlood.Models;
 using iDonateBlood.ViewModels;
+using Microsoft.WindowsAzure.MobileServices;
 using System;
 using System.Collections.Generic;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
 
 // The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
 
@@ -13,6 +16,13 @@ namespace iDonateBlood
     /// </summary>
     public sealed partial class Register : iDonateBlood.Common.LayoutAwarePage
     {
+
+        // MobileServiceCollectionView implements ICollectionView (useful for databinding to lists) and 
+        // is integrated with your Mobile Service to make it easy to bind your data to the ListView
+        private MobileServiceCollectionView<BloodDonors> items;
+
+        private IMobileServiceTable<BloodDonors> bloodDonorTable = App.MobileService.GetTable<BloodDonors>();
+
         public Register()
         {
             this.InitializeComponent();
@@ -40,6 +50,45 @@ namespace iDonateBlood
         /// <param name="pageState">An empty dictionary to be populated with serializable state.</param>
         protected override void SaveState(Dictionary<String, Object> pageState)
         {
+        }
+
+        private void Button_Click_1(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var bloodDonor = new BloodDonors { Email = "ravi.patil@itcube.net", 
+                Address="6th floor, crystal corporate, ITCube, Opp Light House, Bibwewadi-Kondhava Road, Above HDFC Bank, Bibwewadi",
+                City="Pune", NevenDonated=false, BloodGroup="O+ve", Country="India", DateOfBirth=new DateTime(1980, 02,01),
+                FullName ="Ravindra Patil", LastDonatedOn= new DateTime(2011, 01, 01), MobileNumber="+9198542154", 
+                Password="DefaultPassword#1", State="MS", TelephoneNumber="+912023564578" };
+
+            InsertBloodDonor(bloodDonor);
+        }
+
+        private async void InsertBloodDonor(BloodDonors bloodDonor)
+        {
+            // This code inserts a new BloodDonor into the database. When the operation completes
+            // and Mobile Services has assigned an Id, the item is added to the CollectionView
+            await bloodDonorTable.InsertAsync(bloodDonor);
+            items.Add(bloodDonor);
+        
+            SuccessMsg.Text = "Donor Registered successfully.. ID = " + bloodDonor.Id;
+        }
+
+        private void RefreshBloodDonors()
+        {
+            // This code refreshes the entries in the list view be querying the BloodDonors table.
+            // The query excludes completed BloodDonors
+            items = bloodDonorTable
+                .Where(bloodDonor => bloodDonor.NevenDonated == false)
+                .ToCollectionView();
+            //ListItems.ItemsSource = items;
+        }
+
+        private async void UpdateCheckedBloodDonor(BloodDonors item)
+        {
+            // This code takes a freshly completed BloodDonor and updates the database. When the MobileService 
+            // responds, the item is removed from the list 
+            await bloodDonorTable.UpdateAsync(item);
+            items.Remove(item);
         }
 
     }
